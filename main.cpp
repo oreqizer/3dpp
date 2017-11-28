e19ca0629fe70dc5a2a306b86a01f9abd86960fb
oreqizer <oreqizer@gmail.com>
2017-11-28 21:11:50 +0100

    INIT

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <GL/glew.h>
+
+using namespace std;
+
+// Make configs:
+//   brew install cmake
+//   cmake -H. -Bbuild
+// Compile
+//   cmake --build build -- -j3
+int main() {
+    cout << "Yolo" << endl;
+    return 0;
+}
