a6d94dfdbeab7ed2f6ad09683f59c113dcbc5bc0
oreqizer <oreqizer@gmail.com>
2017-12-06 17:06:11 +0100

    display: Add depth buffer

--- src/display.cpp
+++ src/display.cpp
@@ -12,6 +12,8 @@ Display::Display(int width, int height, const std::string& title) {
 	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	// For hidden surface removal
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
 	// Allocates memory for a 2nd, virtual window
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
@@ -38,6 +40,12 @@ Display::Display(int width, int height, const std::string& title) {
 	}
 
 	m_isClosed = false;
+
+	// Hidden surface removal
+	glEnable(GL_DEPTH_TEST);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 }
 
 Display::~Display() {
@@ -60,7 +68,7 @@ void Display::Update() {
 
 void Display::Clear(float r, float g, float b, float a) {
 	glClearColor(r, g, b, a);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 bool Display::IsClosed() {
